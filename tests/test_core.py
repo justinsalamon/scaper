b47433e5994f74734c67e3b6b2ba5726b22c2ce3
Justin Salamon <justin.salamon@gmail.com>
2016-10-03 15:48:12 -0400

    Update test_scaper based on new behavior of scaper.init()

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,68 +1,94 @@
 
-import scaper.core as core
+# import scaper.core as core
+import scaper
 import os
+import pytest
+
+# Paths to fixture files for testing
+FG_PATH = 'tests/data/audio/foreground'
+BG_PATH = 'tests/data/audio/background'
 
 
-def test_scaper():
+def test_scaper(recwarn):
+    '''
+    Test creation of Scaper object.
+    '''
 
-    # no arg
-    sc = core.Scaper()
-    assert sc.bg_path == '../audio/bg'
-    assert sc.fg_path == '../audio/fg'
+    # no args
+    sc = pytest.warns(UserWarning, scaper.Scaper)
+    for x in recwarn:
+        print(x.message)
+    assert len(recwarn) == 2
+    assert sc.fg_path is None
+    assert sc.bg_path is None
+
     # one arg
-    sc = core.Scaper('../audio/fg')
-    assert sc.bg_path == '../audio/bg'
-    assert sc.fg_path == '../audio/fg'
+    sc = pytest.warns(UserWarning, scaper.Scaper, fg_path=FG_PATH)
+    assert len(recwarn) == 3
+    assert sc.fg_path == FG_PATH
+    assert sc.bg_path is None
+
     # two arg
-    sc = core.Scaper('../audio/fg', '../audio/bg')
-    assert sc.bg_path == '../audio/bg'
-    assert sc.fg_path == '../audio/fg'
+    sc = scaper.Scaper(FG_PATH, BG_PATH)
+    assert sc.fg_path == FG_PATH
+    assert sc.bg_path == BG_PATH
+    assert len(recwarn) == 3
+
     # key value args
-    sc = core.Scaper(fg_path='../audio/fg', bg_path='../audio/bg')
-    assert sc.bg_path == '../audio/bg'
-    assert sc.fg_path == '../audio/fg'
-    # paths dont exist
-    sc = core.Scaper(fg_path='../audio/wrong', bg_path='../audio/wwrong')
-    assert sc.bg_path == '../audio/bg'
-    assert sc.fg_path == '../audio/fg'
+    sc = scaper.Scaper(fg_path=FG_PATH,
+                       bg_path=BG_PATH)
+    assert sc.fg_path == FG_PATH
+    assert sc.bg_path == BG_PATH
+    assert len(recwarn) == 3
+
+    # Paths to nonexistent folders should raise warnings.
+    # Folders should remain set to None.
+    # sc = scaper.Scaper(fg_path='data/audio/wrong', bg_path='data/audio/wwrong')
+
+    sc = pytest.warns(UserWarning, scaper.Scaper,
+                      fg_path='tests/data/audio/wrong',
+                      bg_path='tests/data/audio/wwrong')
+    assert sc.fg_path is None
+    assert sc.bg_path is None
+    assert len(recwarn) == 5
 
 
 def test_scaperspec():
 
-    sc = core.Scaper()
+    sc = scaper.Scaper()
 
     # no arg
-    sp = core.ScaperSpec(sc=None)
+    sp = scaper.ScaperSpec(sc=None)
     assert sp.sc is not None
     assert sp.bg_label is not None
     assert sp.bg_duration is not None
     # one arg
-    sp = core.ScaperSpec(8)
+    sp = scaper.ScaperSpec(8)
     assert sp.bg_label is not None
     # two arg, no list
-    sp = core.ScaperSpec(sc, 'music')
+    sp = scaper.ScaperSpec(sc, 'music')
     assert sp.bg_label is not None
     # three arg
-    sp = core.ScaperSpec(sc, ['music'], 1)
+    sp = scaper.ScaperSpec(sc, ['music'], 1)
     assert sp.bg_label is not None
     # key value args, list of bg labels
-    sp = core.ScaperSpec(
+    sp = scaper.ScaperSpec(
         scape=sc, bg_label=['music', 'car', 'crowd'], bg_duration=9)
     assert sp.bg_label == ['music'] or ['crowd'] or ['car']
     assert sp.bg_duration == 9
 
     # wrong label, invalid duration
-    sp = core.ScaperSpec(sc, bg_label='wrong', bg_duration=-3)
+    sp = scaper.ScaperSpec(sc, bg_label='wrong', bg_duration=-3)
     assert sp.bg_label != 'wrong'
     assert sp.bg_duration >= 0
 
 
 def test_add_events():
 
-    sc = core.Scaper()
+    sc = scaper.Scaper()
 
     # no args
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events()
     assert sp.labels is not None
     assert sp.fg_start_times is not None
@@ -73,7 +99,7 @@ def test_add_events():
         len(sp.labels) == len(sp.fg_durations) == len(sp.fg_start_times) ==
         len(sp.snrs) == sp.num_events)
     # one arg
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(['horn'])
     assert sp.labels == ['horn']
     assert sp.fg_start_times is not None
@@ -84,7 +110,7 @@ def test_add_events():
         len(sp.labels) == len(sp.fg_durations) == len(sp.fg_start_times) ==
         len(sp.snrs) == sp.num_events)
     # two arg
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(['siren'], [1])
     assert sp.fg_start_times == [1]
     # assert sp.labels == ['siren']
@@ -95,7 +121,7 @@ def test_add_events():
         len(sp.labels) == len(sp.fg_durations) == len(sp.fg_start_times) ==
         len(sp.snrs) == sp.num_events)
     # three arg
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(['siren'], [1], [2])
     assert sp.fg_start_times == [1]
     # assert sp.labels == ['siren']
@@ -106,14 +132,14 @@ def test_add_events():
         len(sp.labels) == len(sp.fg_durations) == len(sp.fg_start_times) ==
         len(sp.snrs) == sp.num_events)
     # four arg
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(['siren'], [1], [2], [-4])
     assert sp.fg_start_times == [1]
     # assert sp.labels == ['siren']
     # assert sp.fg_durations == [2]
     # assert sp.snrs == [-4]
     # five arg
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(['siren'], [1], [2], [-4], 1)
     assert sp.fg_start_times == [1]
     # assert sp.labels == ['siren']
@@ -122,28 +148,28 @@ def test_add_events():
 
     # collapse these vvv
     # snrs
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(snrs=-1)
     assert sp.snrs == [-1]
 
     # fg_start_times
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(fg_start_times=1)
     assert sp.fg_start_times == [1]
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(fg_start_times=[1, 2, 3], num_events=4)
     assert sp.fg_start_times != [1, 2, 3]
 
     # fg_durations
     sp.add_events(fg_durations=1)
     assert sp.fg_durations == [1]
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(fg_durations=[1, 2, 3], num_events=2)
     assert sp.fg_durations != [1, 2, 3]
 
     # invalid labels, invalid start times, invalid durations,
     # invalid snrs more start times then events
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(
         labels=['foo', 'bar'], fg_start_times=[-200, -200, 0],
         fg_durations=[-1, 0, 3], snrs=[1, 4, 5], num_events=3)
@@ -155,34 +181,34 @@ def test_add_events():
     assert sp.num_events == 3
 
     # duration checks
-    sp = core.ScaperSpec(sc, bg_label=['music'], bg_duration=10)
+    sp = scaper.ScaperSpec(sc, bg_label=['music'], bg_duration=10)
     sp.add_events(
         labels=['music'], fg_start_times=[5, 6, 7, 8], fg_durations=[6],
         snrs=[-1, -3], num_events=5)
     assert len(sp.fg_durations) == 5
 
     # invalid number of events
-    sp = core.ScaperSpec(sc, bg_label=['siren'], bg_duration=10)
+    sp = scaper.ScaperSpec(sc, bg_label=['siren'], bg_duration=10)
     sp.add_events(
         labels=['horn'], fg_start_times=[5, 6, 7, 8], fg_durations=[6],
         snrs=[-1, -3], num_events=-1)
     assert len(sp.fg_durations) == 1
 
     # list extension checks
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(labels=['foo', 'bar'], fg_start_times=[2, 2, 0],
                   fg_durations=[1, 2, 3], snrs=[1, 4, 5], num_events=5)
     assert sp.num_events is not None
 
     # more params than events
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(
         labels='siren', fg_start_times=[1, 2], fg_durations=[1, 2, 3],
         snrs=[1, 4, 5], num_events=1)
     assert sp.num_events is not None
 
     # less events than params
-    sp = core.ScaperSpec(sc)
+    sp = scaper.ScaperSpec(sc)
     sp.add_events(
         labels=['siren', 'siren'], fg_start_times=[1, 2],
         fg_durations=[1, 2, 3], snrs=[1, 4, 5], num_events=1)
@@ -191,8 +217,8 @@ def test_add_events():
 
 def test_generate_jams():
 
-    sc = core.Scaper()
-    sp = core.ScaperSpec(sc, bg_label=['crowd'], bg_duration=10)
+    sc = scaper.Scaper()
+    sp = scaper.ScaperSpec(sc, bg_label=['crowd'], bg_duration=10)
     sp.add_events(
         labels=['horn', 'siren'], fg_start_times=[3, 2], fg_durations=[1, 1],
         snrs=[-2, -5], num_events=2)
@@ -205,8 +231,8 @@ def test_generate_jams():
 
 def test_generate_soundscapes():
 
-    sc = core.Scaper()
-    sp = core.ScaperSpec()
+    sc = scaper.Scaper()
+    sp = scaper.ScaperSpec()
     sp.add_events(
         labels=['horn'], fg_start_times=[2], fg_durations=[1], snrs=[-5],
         num_events=2)
@@ -225,7 +251,7 @@ def test_generate_soundscapes():
     assert the_jam
 
     # incorrect filepath
-    sp = core.ScaperSpec(sc, bg_label=['crowd'], bg_duration=10)
+    sp = scaper.ScaperSpec(sc, bg_label=['crowd'], bg_duration=10)
     sp.add_events(labels=['horn', 'siren'], fg_start_times=[3, 2],
                   fg_durations=[1, 1], snrs=[-2, -5], num_events=2)
     the_jam = sp.generate_jams(sp.spec, 'tests/tmp/dummy_outfile.jams')
