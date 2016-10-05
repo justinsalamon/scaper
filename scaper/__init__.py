a57f7c43bc7749de5acd42b6db95d77074308cef
Justin Salamon <justin.salamon@gmail.com>
2016-10-04 21:09:05 -0400

    Add sound_event namespace to jams during init

--- scaper/__init__.py
+++ scaper/__init__.py
@@ -2,7 +2,11 @@
 """Top-level module for scaper"""
 
 from .core import *
+import jams
+from pkg_resources import resource_filename
 
 __version__ = '0.1.0'
 
-
+# Add sound_event namesapce
+namespace_file = resource_filename(__name__, 'namespaces/sound_event.json')
+jams.schema.add_namespace(namespace_file)
