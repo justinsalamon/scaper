60c4750408f2ea4d1914a17ef3d3d898327db9f7
Justin Salamon <justin.salamon@gmail.com>
2016-10-12 15:20:16 -0400

    Rename test_scaper to test_scaper_init and update based on new init func

--- tests/test_core.py
+++ tests/test_core.py
@@ -14,49 +14,28 @@ FB_LABELS = ['car_horn', 'human_voice', 'siren']
 BG_LABELS = ['park', 'restaurant', 'street']
 
 
-def test_scaper(recwarn):
+def test_scaper_init(recwarn):
     '''
     Test creation of Scaper object.
     '''
 
     # bad duration
-    sc = pytest.raises(ScaperError, scaper.Scaper, -5)
+    sc = pytest.raises(ScaperError, scaper.Scaper, -5, FG_PATH, BG_PATH)
 
-    # only duration
-    sc = pytest.warns(UserWarning, scaper.Scaper, 10.0)
-    # for x in recwarn:
-    #     print(x.message)
-    assert len(recwarn) == 2
-    assert sc.fg_path is None
-    assert sc.bg_path is None
-
-    # duration and fg_path
-    sc = pytest.warns(UserWarning, scaper.Scaper, 10.0, fg_path=FG_PATH)
-    assert len(recwarn) == 3
-    assert sc.fg_path == FG_PATH
-    assert sc.bg_path is None
-
-    # all args
+    # all args valid
     sc = scaper.Scaper(10.0, FG_PATH, BG_PATH)
     assert sc.fg_path == FG_PATH
     assert sc.bg_path == BG_PATH
-    assert len(recwarn) == 3
 
-    # key value args
-    sc = scaper.Scaper(10.0,
-                       fg_path=FG_PATH,
-                       bg_path=BG_PATH)
-    assert sc.fg_path == FG_PATH
-    assert sc.bg_path == BG_PATH
-    assert len(recwarn) == 3
+    # bad fg path
+    sc = pytest.raises(ScaperError, scaper.Scaper, 10.0,
+                       'tests/data/audio/wrong',
+                       BG_PATH)
 
-    # bad fg and bg paths
-    sc = pytest.warns(UserWarning, scaper.Scaper, 10.0,
-                      fg_path='tests/data/audio/wrong',
-                      bg_path='tests/data/audio/wwrong')
-    assert sc.fg_path is None
-    assert sc.bg_path is None
-    assert len(recwarn) == 5
+    # bad bg path
+    sc = pytest.raises(ScaperError, scaper.Scaper, 10.0,
+                       FG_PATH,
+                       'tests/data/audio/wrong')
 
     # ensure fg_labels and bg_labels populated properly
     sc = scaper.Scaper(10.0, FG_PATH, BG_PATH)
