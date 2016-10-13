0901aaaf0c063994eded8a80a09daa0dc5b55920
Justin Salamon <justin.salamon@gmail.com>
2016-10-13 19:28:15 -0400

    Define a ScaperWarning

---
+++ scaper/warnings.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# CREATED: 10/13/16 7:08 PM by Justin Salamon <justin.salamon@nyu.edu>
+
+'''Warning classes for Scaper'''
+
+
+class ScaperWarning(Warning):
+    '''The root Scaper warning class'''
+    pass
