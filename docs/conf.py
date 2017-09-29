2dc58f93a4893ba1e18b997466af827a1a16ac0b
Justin Salamon <justin.salamon@gmail.com>
2017-09-29 17:54:47 -0400

    only mock scaper

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ class Mock(MagicMock):
 
 
 MOCK_MODULES = [
-    'sox', 'jams', 'scipy', 'numpy', 'pandas', 'scaper', 'scaper.core', 'scaper.core.Scaper'
+    'sox', 'jams', 'scipy', 'numpy', 'pandas', 'scaper'
 ]
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
