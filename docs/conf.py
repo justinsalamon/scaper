4c081e3e09639ba723d975e27491ce0ab3e279c1
Justin Salamon <justin.salamon@gmail.com>
2017-10-05 14:46:40 -0400

    Hide EventSpec namedtuple from docs (output)

--- docs/conf.py
+++ docs/conf.py
@@ -377,19 +377,18 @@ texinfo_documents = [
 # texinfo_no_detailmenu = False
 
 
-# def no_namedtuple_attrib_docstring(app, what, name,
-#                                    obj, options, lines):
-#     is_namedtuple_docstring = (
-#         len(lines) == 1 and
-#         lines[0].startswith('Alias for field number')
-#     )
-#     if is_namedtuple_docstring:
-#         # We don't return, so we need to purge in-place
-#         del lines[:]
-#
-#
-# def setup(app):
-#     app.connect(
-#         'autodoc-process-docstring',
-#         no_namedtuple_attrib_docstring,
-#     )
+def maybe_skip_member(app, what, name, obj, skip, options):
+    # print(app)
+    # print(what)
+    # print(name)
+    # print(obj)
+    # print(skip)
+    # print(options)
+    if name == 'EventSpec' or name[0] == '_':
+        return True
+    else:
+        return False
+
+
+def setup(app):
+    app.connect('autodoc-skip-member', maybe_skip_member)
