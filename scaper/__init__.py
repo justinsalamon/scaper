6dab6afca569ee38ad98d9b37f39dd44b3c120be
Justin Salamon <justin.salamon@gmail.com>
2018-07-28 21:04:28 -0400

    use new scaper namespace, no need to import custom sound_event

--- scaper/__init__.py
+++ scaper/__init__.py
@@ -4,10 +4,4 @@
 from .core import Scaper
 from .core import generate_from_jams
 from .core import trim
-import jams
-from pkg_resources import resource_filename
 from .version import version as __version__
-
-# Add sound_event namesapce
-namespace_file = resource_filename(__name__, 'namespaces/sound_event.json')
-jams.schema.add_namespace(namespace_file)
