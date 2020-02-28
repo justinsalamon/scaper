4abc3b2a512147757edba160fe91a1a57c11128a
pseeth <prem@u.northwestern.edu>
2020-02-28 17:00:38 -0600

    fixing comment

--- scaper/core.py
+++ scaper/core.py
@@ -1723,8 +1723,7 @@ class Scaper(object):
                         cmb.convert(samplerate=self.sr,
                                     n_channels=self.n_channels,
                                     bitdepth=None)
-                        # Then trim, using the duration as the lower limit to handle
-                        # backgrounds that are too short
+                        # Then trim
                         cmb.trim(e.value['source_time'],
                                  e.value['source_time'] +
                                  e.value['event_duration'])
